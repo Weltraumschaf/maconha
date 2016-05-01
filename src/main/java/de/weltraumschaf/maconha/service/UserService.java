c7d3395fb6a9a5a505e287cd29965d8ee8e5169e
Sven Strittmatter <sven.strittmatter@iteratec.de>
2016-05-01 21:29:38 +0200

    Reformatting.

--- src/main/java/de/weltraumschaf/maconha/service/UserService.java
+++ src/main/java/de/weltraumschaf/maconha/service/UserService.java
@@ -4,24 +4,22 @@ import java.util.List;
 
 import de.weltraumschaf.maconha.model.User;
 
-
-
 public interface UserService {
-	
-	User findById(long id);
-	
-	User findByName(String name);
-	
-	void saveUser(User user);
-	
-	void updateUser(User user);
-	
-	void deleteUserById(long id);
 
-	List<User> findAllUsers(); 
-	
-	void deleteAllUsers();
-	
-	public boolean isUserExist(User user);
-	
+    User findById(long id);
+
+    User findByName(String name);
+
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUserById(long id);
+
+    List<User> findAllUsers();
+
+    void deleteAllUsers();
+
+    public boolean isUserExist(User user);
+
 }
