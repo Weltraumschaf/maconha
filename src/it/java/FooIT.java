ce5f597e40c6264d231bdda416d0755fe9bcfb5e
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-03-30 08:59:11 +0200

    Add example integration test.

---
+++ src/it/java/FooIT.java
@@ -0,0 +1,32 @@
+import de.weltraumschaf.maconha.MaconhaApplication;
+import de.weltraumschaf.maconha.service.JobService;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Example integration test case.
+ */
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {MaconhaApplication.class})
+public class FooIT {
+
+    @Autowired
+    private JobService jobs;
+
+    @Test
+    @Ignore
+    public void foo() {
+        assertThat(jobs, is(not(nullValue())));
+    }
+}
