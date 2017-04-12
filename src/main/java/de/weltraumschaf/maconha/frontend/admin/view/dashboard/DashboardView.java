890f1dd9e2b803034c0547a81b7e2918b8740016
Sven Strittmatter <ich@weltraumschaf.de>
2017-04-12 23:07:23 +0200

    Layout the dashboard view.

--- src/main/java/de/weltraumschaf/maconha/frontend/admin/view/dashboard/DashboardView.java
+++ src/main/java/de/weltraumschaf/maconha/frontend/admin/view/dashboard/DashboardView.java
@@ -2,11 +2,12 @@ package de.weltraumschaf.maconha.frontend.admin.view.dashboard;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.Responsive;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Panel;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import de.weltraumschaf.maconha.frontend.admin.view.buckets.BucketsView;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +17,14 @@ import javax.annotation.PostConstruct;
 @SpringView(name = DashboardView.VIEW_NAME)
 public final class DashboardView extends Panel implements View {
     public static final String VIEW_NAME = "";
+    private static final String TITLE_ID = "dashboard-title";
+
+    private final Label titleLabel = new Label("Dashboard");
+    private final VerticalLayout root = new VerticalLayout();
+
+    public DashboardView() {
+        super();
+    }
 
     @Override
     public void enter(final ViewChangeListener.ViewChangeEvent event) {
@@ -24,6 +33,24 @@ public final class DashboardView extends Panel implements View {
 
     @PostConstruct
     public void init() {
-        setContent(new Label("Dashboard"));
+        root.setSizeFull();
+        root.setSpacing(false);
+        root.addStyleName("dashboard-view");
+        setContent(root);
+        Responsive.makeResponsive(root);
+        root.addComponent(buildHeader());
+    }
+
+    private Component buildHeader() {
+        HorizontalLayout header = new HorizontalLayout();
+        header.addStyleName("viewheader");
+
+        titleLabel.setId(TITLE_ID);
+        titleLabel.setSizeUndefined();
+        titleLabel.addStyleName(ValoTheme.LABEL_H1);
+        titleLabel.addStyleName(ValoTheme.LABEL_NO_MARGIN);
+        header.addComponent(titleLabel);
+
+        return header;
     }
 }
