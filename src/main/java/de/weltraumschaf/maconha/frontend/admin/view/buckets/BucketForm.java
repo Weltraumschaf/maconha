aa973ac7eae057121a5004acacf96ac77d0d98e6
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-04-12 18:35:20 +0200

    Autowire dependnecies.

--- src/main/java/de/weltraumschaf/maconha/frontend/admin/view/buckets/BucketForm.java
+++ src/main/java/de/weltraumschaf/maconha/frontend/admin/view/buckets/BucketForm.java
@@ -6,15 +6,13 @@ import com.vaadin.ui.Component;
 import com.vaadin.ui.TextField;
 import de.weltraumschaf.maconha.model.Bucket;
 import de.weltraumschaf.maconha.repo.BucketRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.events.EventBus;
 import org.vaadin.viritin.fields.MTextField;
 import org.vaadin.viritin.form.AbstractForm;
 import org.vaadin.viritin.layouts.MFormLayout;
 import org.vaadin.viritin.layouts.MVerticalLayout;
 
-/**
- *
- */
 @UIScope
 @SpringComponent
 final class BucketForm extends AbstractForm<Bucket> {
@@ -22,15 +20,17 @@ final class BucketForm extends AbstractForm<Bucket> {
     private final BucketRepo buckets;
     private final EventBus.UIEventBus events;
 
-    BucketForm(final BucketRepo buckets, final EventBus.UIEventBus events) {
+    @Autowired
+    public BucketForm(final BucketRepo buckets, final EventBus.UIEventBus events) {
         super(Bucket.class);
         this.buckets = buckets;
         this.events = events;
-        // On save & cancel, publish events that other parts of the UI can listen
+
+        // On save & cancel, publish events that other parts of the UI can listen.
         setSavedHandler(bucket -> {
-            // persist changes
+            // Persist changes.
             buckets.save(bucket);
-            // send the event for other parts of the application
+            // Send the event for other parts of the application.
             events.publish(this, new BucketModifiedEvent(bucket));
         });
         setResetHandler(bucket -> events.publish(this, new BucketModifiedEvent(bucket)));
