c04baee33358d36eb209927b3a0292c467d3f1b0
Sven Strittmatter <ich@weltraumschaf.de>
2017-04-28 00:07:53 +0200

    Add icon for type and highlight matched keywords.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,10 +3,37 @@
         let $list = $("#result");
         let $form = $("form");
         let $error = $("#error");
+        let $query = $("input[name='q']");
 
         function onError(data) {
             $error.show();
             console.error(data);
+        }
+
+        function formatKeywords(keywords) {
+            let query = $query.val().toLowerCase().split(/\s+/);
+            let html = '';
+
+            $.each(keywords, function(index, value) {
+                if (index > 0) {
+                    html += ', ';
+                }
+
+                // FIXME Does always evaluate to false.
+                let isInQuery = $.inArray(value, query) > -1;
+
+                if (isInQuery) {
+                    html += '<strong>';
+                }
+
+                html += value.literal;
+
+                if (isInQuery) {
+                    html += '</strong>';
+                }
+            });
+
+            return html;
         }
 
         function showResults(result) {
@@ -14,7 +41,21 @@
                 $list.append('<li class="list-group-item">Nothing found :-(</li>');
             } else {
                 $.each(result, function(index, value) {
-                    $list.append('<li class="list-group-item">' + value.relativeFileName + '</li>');
+                    let html = '<li class="list-group-item">';
+
+                    if (value.type === 'VIDEO') {
+                        html += '<span class="glyphicon glyphicon-film"></span>'
+                    } else if (value.type === 'AUDIO') {
+                        html += '<span class="glyphicon glyphicon-music"></span>'
+                    } else {
+                        html += '<span class="glyphicon glyphicon-file"></span>'
+                    }
+
+                    html += ' <strong>' + value.relativeFileName + '</strong><br>';
+                    html += '<small>(' + value.bucket.directory + ')</small><br>';
+                    html += 'Keywords: ' + formatKeywords(value.keywords);
+                    html += '</li>';
+                    $list.append(html);
                 });
             }
 
@@ -26,7 +67,7 @@
                 $form.submit(function (event) {
                     event.preventDefault();
                     $list.hide().empty();
-                    $.get(baseUrl + "/search", {"q": $("input[name='q']").val()})
+                    $.get(baseUrl + "/search", {"q": $query.val()})
                         .done(showResults)
                         .fail(onError);
                 });
