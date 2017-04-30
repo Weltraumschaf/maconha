29d61ed7401532453ca5dbf9781bab75323ac6a7
Sven Strittmatter <ich@weltraumschaf.de>
2017-04-30 09:19:44 +0200

    Show total number of keywords in view.

--- src/main/java/de/weltraumschaf/maconha/frontend/admin/view/keywords/KeywordsView.java
+++ src/main/java/de/weltraumschaf/maconha/frontend/admin/view/keywords/KeywordsView.java
@@ -5,6 +5,7 @@ import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
 import de.weltraumschaf.maconha.frontend.admin.view.SubView;
 import de.weltraumschaf.maconha.model.Keyword;
 import de.weltraumschaf.maconha.repo.KeywordRepo;
@@ -15,6 +16,8 @@ import org.vaadin.viritin.fields.MTextField;
 import org.vaadin.viritin.grid.MGrid;
 import org.vaadin.viritin.layouts.MHorizontalLayout;
 import org.vaadin.viritin.layouts.MVerticalLayout;
+
+import java.util.List;
 
 /**
  * View to manage {@link Keyword keywords}.
@@ -28,11 +31,13 @@ public final class KeywordsView extends SubView {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KeywordsView.class);
     private static final String TITLE_ID = "keywords-title";
+    private static final String TOTAL_NUMBER_OF_FOUND_KEYWORDS = "Total number of found keywords: %s";
 
     private final MGrid<Keyword> list = new MGrid<>(Keyword.class)
         .withProperties("id", "literal")
         .withColumnHeaders("ID", "Literal")
         .withFullWidth();
+    private Label totalNumber = new Label(String.format(TOTAL_NUMBER_OF_FOUND_KEYWORDS, 0));
     private MTextField filterByLiteral = new MTextField()
         .withPlaceholder("Filter by literal");
     private final KeywordRepo keywords;
@@ -55,7 +60,7 @@ public final class KeywordsView extends SubView {
 
     private Component buildContent() {
         final MVerticalLayout content = new MVerticalLayout(
-            new MHorizontalLayout(filterByLiteral),
+            new MHorizontalLayout(filterByLiteral), totalNumber,
             list
         ).expand(list);
         listEntities();
@@ -69,14 +74,18 @@ public final class KeywordsView extends SubView {
 
     private void listEntities(final String literalFilter) {
         final String normalizedFilter = literalFilter.trim().toLowerCase();
+        final List<Keyword> found;
 
         if (normalizedFilter.isEmpty()) {
             LOGGER.debug("List all keyword entities.");
-            list.setRows(keywords.findAll());
+             found= keywords.findAll();
         } else {
             final String likeFilter = "%" + normalizedFilter + "%";
             LOGGER.debug("List all keyword entities like {}.", likeFilter);
-            list.setRows(keywords.findByLiteralLike(likeFilter));
+            found = keywords.findByLiteralLike(likeFilter);
         }
+
+        totalNumber.setValue(String.format(TOTAL_NUMBER_OF_FOUND_KEYWORDS, found.size()));
+        list.setRows(found);
     }
 }
