fbcf1bd0d516891842f788fcb3287c674dc65979
Sven Strittmatter <ich@weltraumschaf.de>
2017-05-12 21:15:02 +0200

    Shorter captions in view.

--- src/main/java/de/weltraumschaf/maconha/frontend/admin/view/buckets/BucketsView.java
+++ src/main/java/de/weltraumschaf/maconha/frontend/admin/view/buckets/BucketsView.java
@@ -40,12 +40,12 @@ public final class BucketsView extends SubView {
 
     private MTextField filterByDirectory = new MTextField()
         .withPlaceholder("Filter by directory");
-    private final Button addNew = new MButton(VaadinIcons.PLUS, "Add new bucket", this::add);
-    private final Button edit = new MButton(VaadinIcons.PENCIL, "Edit bucket", this::edit);
-    private final Button delete = new ConfirmButton(VaadinIcons.TRASH, "Delete bucket",
+    private final Button addNew = new MButton(VaadinIcons.PLUS, "Add", this::add);
+    private final Button edit = new MButton(VaadinIcons.PENCIL, "Edit", this::edit);
+    private final Button delete = new ConfirmButton(VaadinIcons.TRASH, "Delete",
         "Are you sure you want to delete the entry?", this::remove);
-    private final Button scan = new MButton(VaadinIcons.COGS, "Scan bucket", this::scan);
-    private final Button schedule = new MButton(VaadinIcons.ALARM, "Schedule scan for bucket", this::schedule);
+    private final Button scan = new MButton(VaadinIcons.COGS, "Scan", this::scan);
+    private final Button schedule = new MButton(VaadinIcons.ALARM, "Schedule", this::schedule);
     private final MGrid<Bucket> list = new MGrid<>(Bucket.class)
         .withProperties("id", "name", "directory")
         .withColumnHeaders("ID", "Name", "Directory")
