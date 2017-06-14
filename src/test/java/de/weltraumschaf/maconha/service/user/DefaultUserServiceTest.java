1390f7197a2ee83ffdcf0e0b3d915f645eda25cb
Sven Strittmatter <ich@weltraumschaf.de>
2017-06-14 14:06:39 +0200

    Add some tests for default user service implementation.

--- src/test/java/de/weltraumschaf/maconha/service/user/DefaultUserServiceTest.java
+++ src/test/java/de/weltraumschaf/maconha/service/user/DefaultUserServiceTest.java
@@ -1,17 +1,36 @@
 package de.weltraumschaf.maconha.service.user;
 
+import de.weltraumschaf.maconha.config.MaconhaConfiguration;
+import de.weltraumschaf.maconha.model.User;
+import de.weltraumschaf.maconha.repo.UserRepo;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Collections;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for {@link DefaultUserService}.
  */
 public final class DefaultUserServiceTest {
+    private final UserRepo users = mock(UserRepo.class);
+    private final DefaultUserService sut = new DefaultUserService(users, new MaconhaConfiguration());
+
     @Test
-    @Ignore
-    public void foo() {
+    public void isThereNoAdminUser_trueIfNoUserPresent() {
+        when(users.findByAdmin(true)).thenReturn(Collections.emptyList());
+
+        assertThat(sut.isThereNoAdminUser(), is(true));
+    }
+
+    @Test
+    public void isThereNoAdminUser_falseIfUserPresent() {
+        when(users.findByAdmin(true)).thenReturn(Collections.singletonList(new User()));
+
+        assertThat(sut.isThereNoAdminUser(), is(false));
     }
 }
