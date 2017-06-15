ceb515e4371ebde9c7160f9c982cf342b4b82d43
Sven Strittmatter <ich@weltraumschaf.de>
2017-06-15 14:05:34 +0200

    Write more tests.

--- src/test/java/de/weltraumschaf/maconha/service/scan/batch/JobParamRetrieverTest.java
+++ src/test/java/de/weltraumschaf/maconha/service/scan/batch/JobParamRetrieverTest.java
@@ -2,12 +2,14 @@ package de.weltraumschaf.maconha.service.scan.batch;
 
 import de.weltraumschaf.maconha.service.scan.hashing.HashedFile;
 import org.junit.Test;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.scope.context.StepContext;
 import org.springframework.batch.item.ExecutionContext;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -17,32 +19,12 @@ import static org.junit.Assert.assertThat;
  */
 public final class JobParamRetrieverTest {
 
+    private final BatchContextFactory contexts = new BatchContextFactory();
     private final JobParamRetriever sut = new JobParamRetriever();
-
-    private ChunkContext createContext() {
-        return createContext(Collections.emptyMap());
-    }
-
-    private ChunkContext createContext(final ExecutionContext executionContext) {
-        return createContext(Collections.emptyMap(), executionContext);
-    }
-
-    private ChunkContext createContext(final Map<String, JobParameter> parameters) {
-        return createContext(parameters, new ExecutionContext());
-    }
-
-    private ChunkContext createContext(final Map<String, JobParameter> parameters, final ExecutionContext executionContext) {
-        final JobParameters jobParameters = new JobParameters(parameters);
-        final JobExecution jobExecution = new JobExecution(new JobInstance(42L, "job"), 42L, jobParameters, "job");
-        jobExecution.setExecutionContext(executionContext);
-        final StepExecution stepExecution = new StepExecution("name", jobExecution);
-        final StepContext stepContext = new StepContext(stepExecution);
-        return new ChunkContext(stepContext);
-    }
 
     @Test(expected = IllegalArgumentException.class)
     public void retrieveBucketDirectory_notPresent() {
-        final ChunkContext context = createContext();
+        final ChunkContext context = contexts.createContext();
 
         sut.retrieveBucketDirectory(context);
     }
@@ -51,7 +33,7 @@ public final class JobParamRetrieverTest {
     public void retrieveBucketDirectory_notString() {
         final Map<String, JobParameter> parameters = new HashMap<>();
         parameters.put(JobParameterKeys.BUCKET_DIRECTORY, new JobParameter(42L, false));
-        final ChunkContext context = createContext(parameters);
+        final ChunkContext context = contexts.createContext(parameters);
 
         sut.retrieveBucketDirectory(context);
     }
@@ -60,14 +42,14 @@ public final class JobParamRetrieverTest {
     public void retrieveBucketDirectory() {
         final Map<String, JobParameter> parameters = new HashMap<>();
         parameters.put(JobParameterKeys.BUCKET_DIRECTORY, new JobParameter("dir", false));
-        final ChunkContext context = createContext(parameters);
+        final ChunkContext context = contexts.createContext(parameters);
 
         assertThat(sut.retrieveBucketDirectory(context), is("dir"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void retrieveBucketId_notPresent() {
-        final ChunkContext context = createContext();
+        final ChunkContext context = contexts.createContext();
 
         sut.retrieveBucketId(context);
 
@@ -78,7 +60,7 @@ public final class JobParamRetrieverTest {
     public void retrieveBucketId_NotLong() {
         final Map<String, JobParameter> parameters = new HashMap<>();
         parameters.put(JobParameterKeys.BUCKET_ID, new JobParameter("foo", false));
-        final ChunkContext context = createContext(parameters);
+        final ChunkContext context = contexts.createContext(parameters);
 
         sut.retrieveBucketId(context);
 
@@ -88,7 +70,7 @@ public final class JobParamRetrieverTest {
     public void retrieveBucketId() {
         final Map<String, JobParameter> parameters = new HashMap<>();
         parameters.put(JobParameterKeys.BUCKET_ID, new JobParameter(42L, false));
-        final ChunkContext context = createContext(parameters);
+        final ChunkContext context = contexts.createContext(parameters);
 
         assertThat(sut.retrieveBucketId(context), is(42L));
 
@@ -100,7 +82,7 @@ public final class JobParamRetrieverTest {
         unseenFiles.add(new HashedFile("hash", "file"));
         final ExecutionContext executionContext = new ExecutionContext();
 
-        sut.storeUnseenFiles(createContext(executionContext), unseenFiles);
+        sut.storeUnseenFiles(contexts.createContext(executionContext), unseenFiles);
 
         assertThat(executionContext.get(ContextKeys.UNSEEN_FILES), is(unseenFiles));
     }
@@ -112,6 +94,6 @@ public final class JobParamRetrieverTest {
         final ExecutionContext executionContext = new ExecutionContext();
         executionContext.put(ContextKeys.UNSEEN_FILES, unseenFiles);
 
-        assertThat(sut.retrieveUnseenFiles(createContext(executionContext)), is(unseenFiles));
+        assertThat(sut.retrieveUnseenFiles(contexts.createContext(executionContext)), is(unseenFiles));
     }
 }
