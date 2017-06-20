baebdca6c6583bbdd55f66684848a6cf2e16ff0b
Sven Strittmatter <ich@weltraumschaf.de>
2017-06-20 17:06:20 +0200

    Remove unnecessary class qualifier.

--- src/test/java/de/weltraumschaf/maconha/shell/ResultTest.java
+++ src/test/java/de/weltraumschaf/maconha/shell/ResultTest.java
@@ -7,38 +7,38 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * Tests for {@link de.weltraumschaf.maconha.shell.Result}.
+ * Tests for {@link Result}.
  */
 public class ResultTest {
 
     @Test
     public void equalsAndHashCode() {
-        EqualsVerifier.forClass(de.weltraumschaf.maconha.shell.Result.class).verify();
+        EqualsVerifier.forClass(Result.class).verify();
     }
 
     @Test(expected = NullPointerException.class)
     @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public void constructor_stdoutMustNotBeNull() {
-        new de.weltraumschaf.maconha.shell.Result(0, null, "");
+        new Result(0, null, "");
     }
 
     @Test(expected = NullPointerException.class)
     @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public void constructor_stderrMustNotBeNull() {
-        new de.weltraumschaf.maconha.shell.Result(0, "", null);
+        new Result(0, "", null);
     }
 
     @Test
     public void isSuccessful() {
-        assertThat(new de.weltraumschaf.maconha.shell.Result(0, "", "").isSuccessful(), is(true));
-        assertThat(new de.weltraumschaf.maconha.shell.Result(-1, "", "").isSuccessful(), is(false));
-        assertThat(new de.weltraumschaf.maconha.shell.Result(1, "", "").isSuccessful(), is(false));
+        assertThat(new Result(0, "", "").isSuccessful(), is(true));
+        assertThat(new Result(-1, "", "").isSuccessful(), is(false));
+        assertThat(new Result(1, "", "").isSuccessful(), is(false));
     }
 
     @Test
     public void isFailed() {
-        assertThat(new de.weltraumschaf.maconha.shell.Result(0, "", "").isFailed(), is(false));
-        assertThat(new de.weltraumschaf.maconha.shell.Result(-1, "", "").isFailed(), is(true));
+        assertThat(new Result(0, "", "").isFailed(), is(false));
+        assertThat(new Result(-1, "", "").isFailed(), is(true));
         assertThat(new Result(1, "", "").isFailed(), is(true));
     }
 }
